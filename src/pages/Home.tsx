2450cae42cc9cc590818262b2d1b5ac636090e3f
Bernardxinho <abernardo2001@gmail.com>
2023-12-30 23:15:55 +0000

    pokemon API xD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   IonContent,
   IonHeader,
@@ -7,25 +7,29 @@ import {
   IonToolbar,
   IonInput,
   IonButton,
-} from '@ionic/react';
-import './Home.css';
+} from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const [pokemonName, setPokemonName] = useState<string>("");
+  const [pokemonData, setPokemonData] = useState<any | null>(null);
+  
   const handleSearchClick = async () => {
-    
-    try{
-      const response = await fetch(`https://api.trustpilot.com/v1/business-units/find`)
-
-      if(response.ok){
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+      );
+      if (response.ok) {
         const data = await response.json();
-
-        console.log("API:", data);
-      }else{
-        console.log("error og ythe nirgbeui");
+        console.log("PokeAPI response:", data);
+        setPokemonData(data);
+      } else {
+        console.error("PokeAPI error:", response.statusText);
+        setPokemonData(null);
       }
-
-    }catch(err){
-      console.log(err);
+    } catch (error) {
+      console.error("Error:", error);
+      setPokemonData(null);
     }
   };
 
@@ -37,12 +41,26 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding">
-        <IonInput placeholder="Put here your store"></IonInput>
-
+        <IonInput
+          placeholder="Enter a Pokemon name"
+          value={pokemonName}
+          onIonChange={(e) => setPokemonName(e.detail.value!)}
+        ></IonInput>
         <IonButton expand="full" onClick={handleSearchClick}>
           Search
         </IonButton>
 
+        {pokemonData && (
+          <div>
+            <h2>{pokemonData.name}</h2>
+            {pokemonData.sprites && (
+              <img
+                src={pokemonData.sprites.front_default}
+                alt={`Sprite of ${pokemonData.name}`}
+              />
+            )}
+          </div>
+        )}
       </IonContent>
     </IonPage>
   );
